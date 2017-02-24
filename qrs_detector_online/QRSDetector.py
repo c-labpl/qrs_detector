0d0e7d2efe3bd6fe6822ffa3e65e1e16919374fe
Michał Sznajder <msznajder@gmail.com>
2017-02-24 19:06:48 +0100

    Refactored extract_peaks.

--- qrs_detector_online/QRSDetector.py
+++ qrs_detector_online/QRSDetector.py
@@ -12,6 +12,10 @@ class QRSDetector(object):
 
         # Parameters.
         self.cycling_window = 200  # samples
+        self.filter_lowcut = 0.0  # band pass filter low cut value
+        self.filter_highcut = 15.0  # band pass filter high cut value
+        self.signal_freq = 255  # signal frequency
+        self.integration_window = 15  # signal integration window length in samples
 
         # Detection description.
         self.detected_beat_indicator = 0
@@ -31,15 +35,6 @@ class QRSDetector(object):
 
 
 
-        # General params.
-        self.signal_freq = 255  # signal frequency
-
-        # Filtering parameters.
-        self.filter_lowcut = 0.0  # band pass filter low cut value
-        self.filter_highcut = 15.0  # band pass filter high cut value
-
-        # Integration window.
-        self.integration_window = 15  # signal integration window length in samples
 
         # Realtime params.
         self.refractory_period = 120  # samples
@@ -47,12 +42,6 @@ class QRSDetector(object):
 
         self.peak_timestamp = 100000000000  # seconds
         self.last_peak_timestamp = 0
-
-        # Signal processing variables.
-        self.filtered = np.array([])
-        self.differentiated_signal = np.array([])
-        self.squared_signal = np.array([])
-        self.integrated_signal = np.array([])
 
         # Integrated signal detection and thresholding params.
         self.spk_i = 0.0
@@ -65,7 +54,6 @@ class QRSDetector(object):
         # Peak thresholding variables.
         self.qrs_peak_i = np.array([])
         self.noise_peak_i = np.array([])
-
 
         # Data logger set up.
         self.logger = Logger("QRS", " ", "timestamp", "ecg", "beat_detected", "ibi")
@@ -128,54 +116,55 @@ class QRSDetector(object):
         self.extract_peaks()
 
 
-
-
-
     def extract_peaks(self):
         """Proceses received data."""
+
+        # TODO: All parameters to go to init.
         # Signal filtering - band pass 0-15 Hz.
-        self.filtered_signal = self.bandpass_filter(self.most_recent_measurements, lowcut=self.filter_lowcut,
+        filtered_signal = self.bandpass_filter(self.most_recent_measurements, lowcut=self.filter_lowcut,
                                                     highcut=self.filter_highcut, signal_freq=self.signal_freq,
                                                     filter_order=1)
 
         # Derivative - provides QRS slope info.
-        self.differentiated_signal = np.ediff1d(self.filtered_signal)
+        differentiated_signal = np.ediff1d(filtered_signal)
 
         # Squaring.
-        self.squared_signal = self.differentiated_signal ** 2
+        squared_signal = differentiated_signal ** 2
 
         # Moving-window integration.
-        self.integrated_signal = np.convolve(self.squared_signal, np.ones(self.integration_window))
+        integrated_signal = np.convolve(squared_signal, np.ones(self.integration_window))
 
         # Fiducial mark - peak detection - integrated signal.
-        self.peaks_indices = self.findpeaks(self.integrated_signal, limit=0.40, spacing=50)
+        # TODO: All parameters to go to init.
+        detected_peaks_indices = self.findpeaks(integrated_signal, limit=0.40, spacing=50)
 
-        ## Peak detection variables.
-        self.fiducial_mark_idx_i = np.array([])
-        self.fiducial_mark_val_i = np.array([])
+        # TODO: Check whether detected_peaks_indices == fiducial_mark_idx_i - if yes use fiducial_mark_idx_i to create np.array()
+        # TODO: Refactor fiducial_mark_idx_i to detected_peaks_indices and detected_peaks_values.
+        # TODO: Refactor it to one 2D array with idx and vals in one array.
+        fiducial_mark_idx_i = np.array([])
+        fiducial_mark_val_i = np.array([])
+        for detected_peak_index in detected_peaks_indices:
+            fiducial_mark_idx_i = np.append(fiducial_mark_idx_i, detected_peak_index)
+            fiducial_mark_val_i = np.append(fiducial_mark_val_i, integrated_signal[detected_peak_index])
 
-        for peak_index in self.peaks_indices:
-            self.fiducial_mark_idx_i = np.append(self.fiducial_mark_idx_i, peak_index)
-            self.fiducial_mark_val_i = np.append(self.fiducial_mark_val_i, self.integrated_signal[peak_index])
-
-        self.detect_qrs()
-
-
-
+        self.detect_qrs(detected_peaks_indices=fiducial_mark_idx_i, detected_peaks_values=fiducial_mark_val_i)
 
 
+
+
+    # TODO: Refactor detection.
     # Detection methods.
-    def detect_qrs(self):
+    def detect_qrs(self, detected_peaks_indices, detected_peaks_values):
         """Thresholding detected peaks - integrated - signal."""
         # Check whether refractory period has passed.
         # After a valid QRS complex detection, there is a 200 ms refractory period before the next one can be detected.
         if self.qrs_interval > self.refractory_period:
 
             # Check whether any peak was detected in analysed samples window.
-            if len(self.fiducial_mark_idx_i) > 0:
+            if len(detected_peaks_indices) > 0:
 
                 # Take the last one detected in analysed samples window.
-                peak_idx_i, peak_val_i = self.fiducial_mark_idx_i[-1], self.fiducial_mark_val_i[-1]
+                peak_idx_i, peak_val_i = detected_peaks_indices[-1], detected_peaks_values[-1]
 
                 # Check whether detected peak occured within defined window from end of samples buffer - making sure that the same peak will not be detected twice.
                 if peak_idx_i > self.cycling_window - self.buffer_detection_window:
@@ -213,6 +202,7 @@ class QRSDetector(object):
         else:
             self.logger.log(str(self.timestamp), str(self.measurement), str(self.detected_beat_indicator),
                             str(self.timestamp - self.peak_timestamp))
+
 
     def handle_detection(self):
         print "Pulse"
